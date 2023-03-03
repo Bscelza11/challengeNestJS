462b5b61a03a17579867b43ba6bb2a9a1819ede0
Bscelza11 <bscelza11@gmail.com>
2023-03-03 16:12:04 -0300

    challenge4

--- src/dog/controller/dog/dog.controller.ts
+++ src/dog/controller/dog/dog.controller.ts
@@ -1,46 +1,76 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, ValidationPipe } from '@nestjs/common';
 import {
   Body,
   Delete,
   Param,
-  Patch,
   Post,
   Query,
+  UsePipes,
 } from '@nestjs/common/decorators';
+import { CreateDogDto, UpdateDogDto } from 'src/dog/dto/dog.dto';
 import { DogService } from 'src/dog/services/dog/dog.service';
+import { DogDto } from 'src/dog/dto/dog.dto';
+import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
 
+@ApiTags('Dog')
 @Controller('dog')
 export class DogController {
   constructor(private dogService: DogService) {}
 
   @Get('/findOne')
+  @ApiOkResponse({
+    description: 'The dog age and race',
+    type: DogDto,
+    isArray: false,
+  })
   findOne(@Query('age') age: number, @Query('race') race: string) {
     return this.dogService.findOne(age, race);
   }
   @Get('/:dogId')
+  @ApiOkResponse({
+    description: 'The dog id',
+    type: DogDto,
+    isArray: false,
+  })
   find(@Param('dogId') dogId: number) {
     return this.dogService.find(dogId);
   }
-
   @Get()
+  @ApiOkResponse({
+    description: 'The dogs records',
+    type: DogDto,
+    isArray: true,
+  })
   findAll() {
     return this.dogService.findAll();
   }
 
   @Post()
-  create(
-    @Body('race') race: string,
-    @Body('age') age: number,
-    @Body('color') color: string,
-  ) {
-    return this.dogService.create(age, race, color);
+  @UsePipes(ValidationPipe)
+  @ApiOkResponse({
+    description: 'Create new dog',
+    type: CreateDogDto,
+    isArray: false,
+  })
+  create(@Body() createDogDto: CreateDogDto) {
+    return this.dogService.create(createDogDto);
   }
-
-  @Patch('/:dogId')
-  update(@Param('dogId') dogId: number, @Body('age') age: number) {
-    return this.dogService.update(dogId, age);
+  @Patch(':dogId')
+  @UsePipes(ValidationPipe)
+  @ApiOkResponse({
+    description: 'Update a dog',
+    type: UpdateDogDto,
+    isArray: false,
+  })
+  update(@Body() updateDogDto: UpdateDogDto, @Param('dogId') dogId: number) {
+    return this.dogService.update(updateDogDto, dogId);
   }
-  @Delete('/:dogId')
+  @Delete(':dogId')
+  @ApiOkResponse({
+    description: 'Delete a Dog',
+    type: DogDto,
+    isArray: false,
+  })
   delete(@Param('dogId') dogId: number) {
     return this.dogService.delete(dogId);
   }
---
+++ src/dog/entities/index.ts
@@ -0,0 +1,6 @@
+import { Dog } from './dog.entities';
+
+const entities = [Dog];
+
+export { Dog };
+export default entities;
